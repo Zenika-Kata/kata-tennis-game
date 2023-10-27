9fcd11bc171095a96a04f14c9cf7e345215a550b
Fabien Leite <fabien.leite@zenika.com>
2023-10-27 16:01:21 +0200

    init repo

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+
+    @Test
+    void runner_should_work() {
+        assertThat(1).isEqualTo(2);
+    }
+}
